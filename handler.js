2198e75e098aaa962d404a26b8bafe88913f00fc
Shashika Virajh <v1rhinosa@sph.com.sg>
2023-05-25 02:10:01 +0530

    Create getAllNotes and deleteNote endpoints

--- handler.js
+++ handler.js
@@ -15,3 +15,19 @@ module.exports.updateNote = async (event) => {
     body: JSON.stringify(`Note ${noteId} updated!`),
   };
 };
+
+module.exports.deleteNote = async (event) => {
+  const noteId = event.pathParameters.id
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`Note ${noteId} deleted!`),
+  };
+};
+
+module.exports.getAllNotes = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify('All notes returned!'),
+  };
+};
