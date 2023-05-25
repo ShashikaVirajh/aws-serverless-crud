73de18595f3fad148f3857d69169773d05740510
Shashika Virajh <v1rhinosa@sph.com.sg>
2023-05-25 14:24:23 +0530

    Create notes handler

--- handler.js
+++ handler.js
@@ -1,10 +1,36 @@
 'use strict';
 
-module.exports.createNote = async (event) => {
-  return {
-    statusCode: 201,
-    body: JSON.stringify('A new note created!'),
-  };
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const documentClient = new DynamoDB.documentClient({region: 'us-east-1'});
+
+module.exports.createNote = async (event, context, callback) => {
+  const data = JSON.parse(event.body);
+  console.log({data})
+  
+  try {
+    const params = {
+      TableName: 'notes',
+      Item: {
+        notesId: data.id,
+        title: data.title,
+        body: data.body
+      },
+      ConditionExpression: "attribute_not_exists(notesId)",
+    }
+console.log({params})
+    await documentClient.put(params).promise();
+
+    callback(null, {
+      statusCode: 201,
+      body: JSON.stringify(data),
+    });
+  } catch (error) {
+    console.log({error})
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify(error.message),
+      });
+  }
 };
 
 module.exports.updateNote = async (event) => {
